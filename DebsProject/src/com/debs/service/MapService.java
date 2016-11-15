e3dd3e12cf57ba91e30263be50233a8e8fa51a2f
adelababinciuc <babinciuc.adela@gmail.com>
2016-11-15 23:35:40 +0200

    Services implementation

--- DebsProject/src/com/debs/service/MapService.java
+++ DebsProject/src/com/debs/service/MapService.java
@@ -5,18 +5,23 @@ import com.debs.model.ScoresModel;
 
 public class MapService {
 
-	private ScoresModel scoresModel;
-	
-	public void updatePostScoreMap(Long newScore, Long oldScore, Long postId){
-		
+	private ScoresModel scoresModel = new ScoresModel();
+
+	public void updatePostScoreMap(Long newScore, Long oldScore, Long postId) {
+		if (oldScore == null){
+			scoresModel.getPostScoreMap().get(newScore).add(postId);
+		} else if (!newScore.equals(oldScore)) {
+			scoresModel.getPostScoreMap().get(oldScore).remove(postId);
+			scoresModel.getPostScoreMap().get(newScore).add(postId);
+		}
 	}
-	
-	public void updatePostMap(Post post){
-		
+
+	public void updatePostMap(Post post) {
+		scoresModel.getPostMap().put(post.getId(), post);
 	}
-	
-	public void updateCommentPostMap(Long commentId, Long postId){
-		
+
+	public void updateCommentPostMap(Long commentId, Long postId) {
+		scoresModel.getCommentPostMap().put(commentId, postId);
 	}
 
 	public ScoresModel getScoresModel() {
@@ -26,7 +31,5 @@ public class MapService {
 	public void setScoresModel(ScoresModel scoresModel) {
 		this.scoresModel = scoresModel;
 	}
-	
-	
-	
+
 }
