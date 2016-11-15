4aa8af0c59cb37ef73751e3b0d4d6acb05eea718
Cristian <cristian.muntianu@mambu.com>
2016-11-15 23:37:03 +0200

    Created FileUtils, StreamQueue, added some example files and created tests for the two classes.

--- DebsProject/src/com/debs/utils/StreamQueue.java
+++ DebsProject/src/com/debs/utils/StreamQueue.java
@@ -1,10 +1,25 @@
 package com.debs.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StreamQueue {
 
-	public List<List<String>> divideStreams(List<String> streams){
-		return null;
+	
+
+	public static List<List<String>> divideStreams(List<String> streams, int divisions){
+		List<List<String>> queue;
+		queue = chopped(streams, divisions);
+		return queue;
+	}
+	private static <T> List<List<T>> chopped(List<T> list, final int L) {
+	    List<List<T>> parts = new ArrayList<List<T>>();
+	    final int N = list.size();
+	    for (int i = 0; i < N; i += L) {
+	        parts.add(new ArrayList<T>(
+	            list.subList(i, Math.min(N, i + L)))
+	        );
+	    }
+	    return parts;
 	}
 }
